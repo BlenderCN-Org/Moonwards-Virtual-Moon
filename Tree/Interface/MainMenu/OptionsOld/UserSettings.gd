8b0de88fd53291cb7c04ed2871291988f507dee7
Zach777 <MeteorCity3109@protonmail.com>
2020-04-26 08:56:57 -0400

    UserSettings and HuePicker refactored. - Fix script warnings. - Fix HuePicker anchor values so it scales correctly. - UserSettings now logs events with Log.trace().

--- Tree/Interface/MainMenu/OptionsOld/UserSettings.gd
+++ Tree/Interface/MainMenu/OptionsOld/UserSettings.gd
@@ -8,58 +8,68 @@ enum SLOTS{
 	SHOES
 }
 
+var pants_color : = Color(1,1,1)
+var shirt_color : = Color(1,1,1)
+var skin_color : = Color(1,1,1)
+var hair_color : = Color(1,1,1)
+var shoes_color : = Color(1,1,1)
+
 var current_slot : int = SLOTS.PANTS
 
 # Needs to use the paths before it's ready, so it will crash using onready
 var text_edit1 : String = "VBoxContainer/UsernameContainer/UsernameTextEdit"
-var text_edit2 : String = "VBoxContainer2/UsernameTextEdit2"
+var text_edit2 : String = "ModelDisplay/UsernameTextEdit2"
 var gender_edit : String = "VBoxContainer/Gender"
-var avatar_preview : String = "VBoxContainer2/ViewportContainer/Viewport/AvatarPreview"
-var hue_picker : String = "CenterContainer/HuePicker"
-var button_containter : String = "VBoxContainer2/ViewportContainer"
+var avatar_preview : String = "ModelDisplay/ViewportContainer/Viewport/AvatarPreview"
+var hue_picker : String = "HuePicker"
+var button_containter : String = "ModelDisplay/ViewportContainer"
+
+var username : String = "default username"
+const GENDER_MALE : int = 1
+const GENDER_FEMALE : int = 0
+var gender : int = GENDER_MALE
 
 
 func _ready() -> void:
-	
-	get_node(text_edit1).text = Options.username
-	get_node(text_edit2).text = Options.username
+	get_node(text_edit1).text = username
+	get_node(text_edit2).text = username
 	switch_slot()
-	_on_Gender_item_selected(Options.gender)
-	get_node(gender_edit).selected = int(Options.gender)
+	_on_Gender_item_selected(gender)
+	get_node(gender_edit).selected = gender
 	get_node(button_containter).get_node("Viewport").size = get_node(button_containter).rect_size
 
 func switch_slot() -> void:
 	if current_slot == SLOTS.PANTS:
-		get_node(hue_picker).color = Options.pants_color
+		get_node(hue_picker).color = pants_color
 	elif current_slot == SLOTS.SHIRT:
-		get_node(hue_picker).color = Options.shirt_color
+		get_node(hue_picker).color = shirt_color
 	elif current_slot == SLOTS.SKIN:
-		get_node(hue_picker).color = Options.skin_color
+		get_node(hue_picker).color = skin_color
 	elif current_slot == SLOTS.HAIR:
-		get_node(hue_picker).color = Options.hair_color
+		get_node(hue_picker).color = hair_color
 	elif current_slot == SLOTS.SHOES:
-		get_node(hue_picker).color = Options.shoes_color
-	get_node(avatar_preview).set_colors(Options.pants_color, Options.shirt_color, Options.skin_color, Options.hair_color, Options.shoes_color)
+		get_node(hue_picker).color = shoes_color
+	get_node(avatar_preview).set_colors(pants_color, shirt_color, skin_color, hair_color, shoes_color)
 
 func _on_HuePicker_Hue_Selected(color : Color) -> void:
 	if current_slot == SLOTS.PANTS:
-		Options.pants_color = color
+		 pants_color = color
 	elif current_slot == SLOTS.SHIRT:
-		Options.shirt_color = color
+		 shirt_color = color
 	elif current_slot == SLOTS.SKIN:
-		Options.skin_color = color
+		 skin_color = color
 	elif current_slot == SLOTS.HAIR:
-		Options.hair_color = color
+		 hair_color = color
 	elif current_slot == SLOTS.SHOES:
-		Options.shoes_color = color
-	get_node(avatar_preview).set_colors(Options.pants_color, Options.shirt_color, Options.skin_color, Options.hair_color, Options.shoes_color)
+		 shoes_color = color
+	get_node(avatar_preview).set_colors( pants_color,  shirt_color,  skin_color,  hair_color,  shoes_color)
 
 func _on_CfgPlayer_pressed() -> void:
 	$WindowDialog.popup_centered()
 
 func _on_SaveButton_pressed() -> void:
-	Options.save()
-#	UIManager.back()
+	#Currently just log what the player is doing.
+	Log.trace(self, "_on_SaveButton_pressed", "Save button pressed by player.")
 
 func _on_SlotOption_item_selected(ID : int) -> void:
 	get_node(avatar_preview).clean_selected()
@@ -67,10 +77,9 @@ func _on_SlotOption_item_selected(ID : int) -> void:
 	current_slot = ID
 	switch_slot()
 
-
 func _on_Gender_item_selected(ID : int) -> void:
-	Options.gender = ID
-	get_node(avatar_preview).set_gender(Options.gender)
+	gender = ID
+	get_node(avatar_preview).set_gender(gender)
 	if ID == 0:
 		get_node(button_containter).get_node("Female").show()
 		get_node(button_containter).get_node("Male").hide()
@@ -79,9 +88,11 @@ func _on_Gender_item_selected(ID : int) -> void:
 		get_node(button_containter).get_node("Male").show()
 
 func _on_UsernameTextEdit_text_changed(new_text : String) -> void:
-	Options.username = new_text
+	username = new_text
 	get_node(text_edit2).text = new_text
+	Log.trace(self, "_on_UsernameTextEdit_text_changed", "Change player's name.'")
 
 func _on_UsernameTextEdit2_text_changed(new_text : String) -> void:
-	Options.username = new_text
+	username = new_text
 	get_node(text_edit1).text = new_text
+	Log.trace(self, "_on_UsernameTextEdit2_text_changed", "Change player's name.'")
